450de432a3f82dce656f4c78fd5da81ebe7e3b4e
ramaprabhur <ramaprabhur@fss.co.in>
2020-03-28 11:54:05 +0530

    initial commit

---
+++ src/test/java/com/fss/empdb/EmpdbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fss.empdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpdbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
